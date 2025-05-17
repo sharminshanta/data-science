5c82e447fcfcfe38a9d51e7d0141913305b0b1e9
Sharmin Shanta <mimetyshanta@gmail.com>
2025-05-18 01:47:25 +0600

    #1: Refactored the code.

--- Class-01/problem-01.py
+++ Class-01/problem-01.py
@@ -7,7 +7,6 @@ Output:
 Machine-Learning-With Python and R-Program-Conducted-By-Datasolution360
 '''
 
-
 str1 = "Machine"
 str2 = "Learning"
 str3 = "With Python and R"
@@ -15,5 +14,5 @@ str4 = "Program"
 str5 = "By"
 str6 = "Datasolution360"
 
-str7 = (f"{str1}-{str2}-{str3}-{str4}-Conducted-{str5}-{str6}")
-print(str7)
+result = (f"{str1}-{str2}-{str3}-{str4}-Conducted-{str5}-{str6}")
+print(result)
