3ddf1ba95a88545d41b72d2cc4b3b1963706e1da
Sharmin Shanta <mimetyshanta@gmail.com>
2025-05-18 01:00:07 +0600

    #8: find the sum of 2 fractions.

--- Class-01/problem-08.py
+++ Class-01/problem-08.py
@@ -12,7 +12,7 @@ Repeat until r becomes 0.
 The last non-zero remainder is the GCD.
 '''
 
-
+# Find Greatest Common Divisor
 def findGCD(a, b):
     while b != 0:
         r = a % b
@@ -20,13 +20,14 @@ def findGCD(a, b):
         b = r
     return a
 
+# Find Lest Common Multiplier
 def findLCM(a, b):
     product = a * b
     LCM = product / findGCD(a, b)
 
     return LCM
 
-
+# Addition of Fraction
 def addFraction(n1, d1, n2, d2):
     LCM = findLCM(d1, d2)
     # print(LCM)
@@ -37,9 +38,26 @@ def addFraction(n1, d1, n2, d2):
     return result
 
 
-numerator1 = int(input("Enter first numerator: "))
-denominator1 = int(input("Enter first denominator: "))
-numerator2 = int(input("Enter second numerator: "))
-denominator2 = int(input("Enter second denominator: "))
-result = addFraction(numerator1, denominator1, numerator2, denominator2)
-print(float("{:.3f}".format(result)))
+# numerator1 = int(input("Enter first numerator: "))
+# denominator1 = int(input("Enter first denominator: "))
+# numerator2 = int(input("Enter second numerator: "))
+# denominator2 = int(input("Enter second denominator: "))
+# result = addFraction(numerator1, denominator1, numerator2, denominator2)
+# print(float("{:.3f}".format(result)))
+
+try:
+    print("Enter the first fraction:")
+    numerator1 = int(input("Enter first numerator: "))
+    denominator1 = int(input("Enter first denominator: "))
+
+    print("Enter the second fraction:")
+    numerator2 = int(input("Enter second numerator: "))
+    denominator2 = int(input("Enter second denominator: "))
+
+    if denominator1 == 0 or denominator2 == 0:
+        print("Denominator cannot be zero.")
+    else:
+        result = addFraction(numerator1, denominator1, numerator2, denominator2)
+        print(f"The sum of the fractions is: {float("{:.3f}".format(result))}")
+except ValueError:
+    print("Invalid input! Please enter integers only.")
